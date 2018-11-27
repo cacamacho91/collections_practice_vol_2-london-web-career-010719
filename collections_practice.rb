b0ead89a572ea5d73087c8fc9357b76d8f90dc11
dashing-frame-3501 <cacamacho91@gmail.com>
2018-11-27 13:32:42 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,11 @@
-# your code goes here
+def begins_with_r(array)
+  starts_with_r = true
+
+  array.each do |x|
+    if !(x.starts_with("r"))
+      starts_with_r = false
+      return starts_with_r
+    end
+  end
+  starts_with_r
+end
