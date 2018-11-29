1d8892c060287810f5fd0606f2135b4c757a3329
dashing-frame-3501 <cacamacho91@gmail.com>
2018-11-29 20:14:02 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,34 @@
-# your code goes here
+def begins_with_r(array)
+  array.each do |x|
+    if !(x.start_with?("r"))
+      return false
+    end
+  end
+  true
+end
+
+def contain_a(array)
+  array.select do |x|
+    x.include?("a")
+  end
+end
+
+def first_wa(array)
+  array.find do |x|
+    x.is_a?(String) ? x.start_with?("wa") : false
+  end
+end
+
+def remove_non_strings(array)
+  array.delete_if do |x|
+    !(x.is_a?(String))
+  end
+end
+
+def count_elements(array)
+end
+
+def merge_data(array_1, array_2)
+  combined_array = array_1 << array_2
+  combined_array.flatten!
+end
