353c46b2bc95a22b4d9130efca6a14a7a505229a
dashing-frame-3501 <cacamacho91@gmail.com>
2018-11-29 14:53:53 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,32 @@
-# your code goes here
+def begins_with_r(array)
+  array.each do |x|
+    if !(x.start_with?("r"))
+      return false
+    end
+  end
+  true
+end
+
+def contain_a(array)
+  array.select do |x|
+    x.include?("a")
+  end
+end
+
+def first_wa(array)
+  array.find do |x|
+    x.is_a?(String) ? x.start_with?("wa") : false
+  end
+end
+
+def remove_non_strings(array)
+  array.delete_if do |x|
+    !(x.is_a?(String))
+  end
+end
+
+#to_do
+def count_elements(array)
+  count = Hash.new
+end
+  
